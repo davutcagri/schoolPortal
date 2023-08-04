a652459368d28270d66ac82ca11efc9a4439c73b
davutcagri <71286320+davutcagri@users.noreply.github.com>
2023-08-04 11:09:31 +0300

    new update

---
+++ src/main/java/davutcagri/schoolPortal/exception/TeacherNotFoundException.java
@@ -0,0 +1,10 @@
+package davutcagri.schoolPortal.exception;
+
+public class TeacherNotFoundException extends Exception {
+    public TeacherNotFoundException() {
+    }
+
+    public TeacherNotFoundException(String message) {
+        super(message);
+    }
+}
